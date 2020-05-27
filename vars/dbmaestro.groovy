336a284b446bc65c0279571379f913f66ca0fdae
MaestroPeretz <peretzr@dbmaestro.com>
2020-05-27 10:35:50 +0000

    Update dbmaestro.groovy

--- vars/dbmaestro.groovy
+++ vars/dbmaestro.groovy
@@ -42,6 +42,7 @@ def prepPackageFromGitCommit() {
 		details = commitLine.split(" ")
 		commitType = details[0]
 		commitHash = details[1]
+		commitDesc = details[2..-1].join(" ")
 		
 		// Get the date of the commit
 		stdoutLines = bat([returnStdout: true, script: "git show --pretty=%%cd ${commitHash}"]).trim().split("\n")
@@ -49,7 +50,7 @@ def prepPackageFromGitCommit() {
 		
 		// Get the committer
 		stdoutLines = bat([returnStdout: true, script: "git show --pretty=%%ce ${commitHash}"]).trim().split("\n")
-		commitMail = new Date(stdoutLines[1])
+		commitMail = stdoutLines[1]
 		
 		// Get sql files changed in the commit
 		stdoutLines = bat([returnStdout: true, script: "git diff --name-only ${commitHash} Database\\*.sql"]).trim().split("\n")
@@ -57,7 +58,7 @@ def prepPackageFromGitCommit() {
 			if (changedFile == stdoutLines.first()) continue
 			scriptForPackage = scriptsForPackage.find {it.filePath == changedFile}
 			scriptForPackage.modified = commitDate
-			scriptForPackage.commit = [commitType: commitType, commitHash: commitHash, commitMail: commitMail]
+			scriptForPackage.commit = [commitType: commitType, commitHash: commitHash, commitDesc: commitDesc, commitMail: commitMail]
 		}
 	}
 	
@@ -69,7 +70,7 @@ def prepPackageFromGitCommit() {
 	scripts = []
 	for (item in scriptsForPackage.sort {it.modified} ) {
 		scriptFileName = item.filePath.substring(item.filePath.lastIndexOf("/") + 1)
-		scripts.add([name: scriptFileName, tags: [[tagNames: [item.commit.commitMail, item.commit.commitHash], tagType: "Custom"]]])
+		scripts.add([name: scriptFileName, tags: [[tagNames: [item.commit.commitMail, item.commit.commitHash, item.commit.commitDesc], tagType: "Custom"]]])
 		Files.copy(Paths.get("${env.WORKSPACE}\\${item.filePath}"), Paths.get("${target_dir}\\${scriptFileName}"))
 	}
 	manifest = new JsonBuilder()
