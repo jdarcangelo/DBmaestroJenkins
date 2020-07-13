ce455b5302585a5dfc24199e03914c812f9005e7
Peretz Rickett <peretzr@dbmaestro.com>
2020-07-13 15:15:52 +0300

    Update dbmaestro.groovy

--- vars/dbmaestro.groovy
+++ vars/dbmaestro.groovy
@@ -290,7 +290,7 @@ def generateDriftDashboard() {
 
 			try {
 				echo "Performing Validate on environment ${environment}"
-				result = execCommand("java -jar \"${parameters.jarPath}\" -Validate -ProjectName ${pipeline.name} -EnvName \"${environment}\" -PackageName @CurrentVersion -IgnoreScriptWarnings y -AuthType ${parameters.authType} -Server ${parameters.server} -UserName ${parameters.userName} -Password ${parameters.authToken}")
+				result = execCommand("java -jar \"${parameters.jarPath}\" -Validate -ProjectName ${pipeline.name} -EnvName \"${environment}\" -PackageName @CurrentVersion -IgnoreScriptWarnings y -AuthType ${parameters.authType} -Server ${parameters.server} -UserName ${parameters.userName} -Password ${parameters.authToken}", false)
 				echo "${pipeline.name}.${environment} validated successfully"
 				itsGood = true
 			}
@@ -302,14 +302,14 @@ def generateDriftDashboard() {
 			def url = "http://${parameters.server}:88"
 
 			for (line in result) {
-				//echo "line is: ${line}"
+				echo "line is: ${line}"
 				if (line.contains("Report")) {
-					//echo "contains"
+					echo "contains"
 					url = line.substring(line.indexOf("Report") + 8)
-				}
-				//} else {
-				//	echo "doesn't contain"
 				//}
+				} else {
+					echo "doesn't contain"
+				}
 			}
 
 			reportBuffer << "<td bgcolor=\"${statusColor}\"><a href=\"${url}\">${environment}</a></td>"
